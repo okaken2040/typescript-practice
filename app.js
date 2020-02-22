c2495115c4b0648cd7b12c8f7f304f753442101a
Kensei Okamoto <okamotokensei@okamotokenshounoMacBook-puro.local>
2020-02-22 16:52:42 +0900

    エラーを検出

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+var myName = "太郎";
+myName = 28;
+var pi = 3.14;
+pi = "次郎";
+var hobbies = true;
+hobbies = 1;
---
+++ app.ts
@@ -0,0 +1,6 @@
+let myName = "太郎";
+myName = 28;
+let pi = 3.14;
+pi = "次郎";
+let hobbies = true;
+hobbies = 1;
