b989e03f940c09b4742f4369f11626355a40481a
Kensei Okamoto <okamotokensei@okamotokenshounoMacBook-puro.local>
2020-02-22 16:57:00 +0900

    型を指定する方法

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 "use strict";
-var myName = "太郎";
-myName = 28;
-var pi = 3.14;
-pi = "次郎";
-var hobbies = true;
-hobbies = 1;
+var hello;
+hello = "こんにちは";
+var yes;
+yes = true;
--- app.ts
+++ app.ts
@@ -1,6 +1,4 @@
-let myName = "太郎";
-myName = 28;
-let pi = 3.14;
-pi = "次郎";
-let hobbies = true;
-hobbies = 1;
+let hello: string;
+hello = "こんにちは";
+let yes: boolean;
+yes = true;
